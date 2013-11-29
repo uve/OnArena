7136c17706277b3cc9b68b839377ceed26c3a871
Nikita Grachev <nikita.grachev@gmail.com>
2013-11-29 22:25:23 +0700

    done

--- settings.py
+++ settings.py
@@ -110,8 +110,11 @@ AUTHENTICATION_BACKENDS = (
 
 
 MIDDLEWARE_CLASSES = (
-    #'django.middleware.csrf.CsrfViewMiddleware',    
-    'google.appengine.ext.appstats.recording.AppStatsDjangoMiddleware',
+    #'django.middleware.csrf.CsrfViewMiddleware',
+        
+    #'google.appengine.ext.appstats.recording.AppStatsDjangoMiddleware',
+    
+    
     #'middleware.domain.DomainMiddleware',
     #'middleware.auth.AuthenticationMiddleware',
     #'middleware.exception.ExceptionMiddleware',
